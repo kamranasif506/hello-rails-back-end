a46a83c3d9bd0fc5ec1bb32616df3609e9b026b9
Kamran Asif <98281453+kamranasif506@users.noreply.github.com>
2023-10-25 10:32:13 +0500

    setup rack-cors

---
+++ app/controllers/greetings_controller.rb
@@ -0,0 +1,7 @@
+class GreetingsController < ApplicationController
+    def random
+      message = Message.order("RANDOM()").first
+      render json: { message: message.content }
+    end
+  end
+  
